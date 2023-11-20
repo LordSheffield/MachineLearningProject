9fdca36ca1c372f3e593ac5ee67edc0491d12959
Connor <coastconnor2@gmail.com>
2023-11-20 15:58:38 -0600

    Update2

--- MachineLearningProject.py
+++ MachineLearningProject.py
@@ -17,19 +17,8 @@ POS_PATH = os.path.join('data', 'positive')
 NEG_PATH = os.path.join('data', 'negative')
 ANC_PATH = os.path.join('data', 'anchor')
 
-#Only needs to be run once
-#os.makedirs(POS_PATH)
-#os.makedirs(NEG_PATH)
-#os.makedirs(ANC_PATH)
-
-"""for directory in os.listdir('lfw'):
-    for file in os.listdir(os.path.join('lfw', directory)):
-        EX_PATH = os.path.join('lfw', directory, file)
-        NEW_PATH = os.path.join(NEG_PATH, file)
-        os.replace(EX_PATH, NEW_PATH)"""
-
 #Webcam Collection
-cap = cv2.VideoCapture(0)
+''''cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -52,7 +41,38 @@ while cap.isOpened():
         break
 
 cap.release()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows() '''
+
+anchor = tf.data.Dataset.list_files(ANC_PATH+'\*.jpg').take(300)
+positive = tf.data.Dataset.list_files(POS_PATH+'\*.jpg').take(300)
+negative = tf.data.Dataset.list_files(NEG_PATH+'\*.jpg').take(300)
+
+def preprocess(file_path):
+    byte_img = tf.io.read_file(file_path)
+    img = tf.io.decode_jpeg(byte_img)
+    img = tf.image.resize(img, (100, 100))
+    img = img / 255.0
+    return img
+
+positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
+negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
+data = positives.concatenate(negatives)
+
+def preprocess_twin(input_img, validation_img, label):
+    return(preprocess(input_img), preprocess(validation_img), label)
+
+data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=1024)
+
+train_data = data.take(round(len(data)*.7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+test_data = data.skip(round(len(data)*.7))
+test_data = test_data.take(round(len(data)*.3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
 
 
 
